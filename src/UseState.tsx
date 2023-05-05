b72205fb1c341c4ffb6924fd4fbe94cc9bba38b4
garzonsergio <scgarzonp@gmail.com>
2023-05-05 09:42:36 -0500

    There was created a loading state and effect that finished it in the UseState component

--- src/UseState.tsx
+++ src/UseState.tsx
@@ -4,18 +4,36 @@ interface UseProps {
 }
 
 interface AppState {
-  state: boolean;
+  error: boolean;
+  loading: boolean;
 }
 
 const UseState = ({ name }: UseProps) => {
-  const [error, setError] = React.useState<AppState["state"]>(false);
+  const [error, setError] = React.useState<AppState["error"]>(false);
+  const [loading, setLoading] = React.useState<AppState["loading"]>(false);
+
+  React.useEffect(() => {
+    console.log("Start the effect");
+
+    if (loading) {
+      setTimeout(() => {
+        console.log("Validating");
+        setLoading(false);
+        console.log("Validation completed");
+      }, 3000);
+    }
+
+    console.log("Ending the effect");
+  }, [loading]);
+
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor ingresa el código de seguridad</p>
       {error && <p>Error:El código es incorrecto</p>}
+      {loading && <p>... Loading</p>}
       <input placeholder="Código de Seguridad" />
-      <button onClick={() => setError((e) => !e)}>Comprobar</button>
+      <button onClick={() => setLoading((e) => !e)}>Comprobar</button>
     </div>
   );
 };
