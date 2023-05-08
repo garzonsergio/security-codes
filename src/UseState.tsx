9ce01b5103dd46a7e8e0834c894776699e088707
garzonsergio <scgarzonp@gmail.com>
2023-05-08 09:06:29 -0500

    Validation of error was modified without touching the state

--- src/UseState.tsx
+++ src/UseState.tsx
@@ -6,9 +6,13 @@ interface UseProps {
 interface AppState {
   error: boolean;
   loading: boolean;
+  value: string | number;
 }
 
+const SECURITY_CODE = "203559";
+
 const UseState = ({ name }: UseProps) => {
+  const [value, setValue] = React.useState<AppState["value"]>("");
   const [error, setError] = React.useState<AppState["error"]>(false);
   const [loading, setLoading] = React.useState<AppState["loading"]>(false);
 
@@ -16,9 +20,15 @@ const UseState = ({ name }: UseProps) => {
     console.log("Start the effect");
 
     if (loading) {
+      // setError(false);
       setTimeout(() => {
         console.log("Validating");
         setLoading(false);
+        if (value === SECURITY_CODE) {
+          setLoading(false);
+        } else {
+          setError(true);
+        }
         console.log("Validation completed");
       }, 3000);
     }
@@ -26,13 +36,22 @@ const UseState = ({ name }: UseProps) => {
     console.log("Ending the effect");
   }, [loading]);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor ingresa el código de seguridad</p>
-      {error && <p>Error:El código es incorrecto</p>}
+      {error && !loading && <p>Error:El código es incorrecto</p>}
       {loading && <p>... Loading</p>}
-      <input placeholder="Código de Seguridad" />
+      <input
+        placeholder="Código de Seguridad"
+        value={value}
+        onChange={handleChange}
+      />
       <button onClick={() => setLoading((e) => !e)}>Comprobar</button>
     </div>
   );
