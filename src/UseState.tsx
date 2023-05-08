a340580b23fc6eb4b76f9d488a12efc91526308b
garzonsergio <scgarzonp@gmail.com>
2023-05-08 11:06:54 -0500

    We moved from single States to compose State in UseState components

--- src/UseState.tsx
+++ src/UseState.tsx
@@ -4,40 +4,50 @@ interface UseProps {
 }
 
 interface AppState {
-  error: boolean;
-  loading: boolean;
-  value: string | number;
+  state: {
+    error: boolean;
+    loading: boolean;
+    value: string | number;
+  };
+  // error: boolean;
+  // loading: boolean;
+  // value: string | number;
 }
 
 const SECURITY_CODE = "203559";
 
 const UseState = ({ name }: UseProps) => {
-  const [value, setValue] = React.useState<AppState["value"]>("");
-  const [error, setError] = React.useState<AppState["error"]>(false);
-  const [loading, setLoading] = React.useState<AppState["loading"]>(false);
+  const [state, setState] = React.useState<AppState["state"]>({
+    value: "",
+    error: false,
+    loading: false,
+  });
+  // const [value, setValue] = React.useState<AppState["value"]>("");
+  // const [error, setError] = React.useState<AppState["error"]>(false);
+  // const [loading, setLoading] = React.useState<AppState["loading"]>(false);
 
   React.useEffect(() => {
     console.log("Start the effect");
 
-    if (loading) {
+    if (state.loading) {
       // setError(false);
       setTimeout(() => {
         console.log("Validating");
-        setLoading(false);
-        if (value === SECURITY_CODE) {
-          setLoading(false);
+        setState({ ...state, loading: false });
+        if (state.value === SECURITY_CODE) {
+          setState({ ...state, loading: false, error: false });
         } else {
-          setError(true);
+          setState({ ...state, loading: false, error: true });
         }
         console.log("Validation completed");
       }, 3000);
     }
 
     console.log("Ending the effect");
-  }, [loading]);
+  }, [state.loading]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    setState({ ...state, value: e.target.value });
     console.log(e.target.value);
   };
 
@@ -45,14 +55,16 @@ const UseState = ({ name }: UseProps) => {
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor ingresa el código de seguridad</p>
-      {error && !loading && <p>Error:El código es incorrecto</p>}
-      {loading && <p>... Loading</p>}
+      {state.error && !state.loading && <p>Error:El código es incorrecto</p>}
+      {state.loading && <p>... Loading</p>}
       <input
         placeholder="Código de Seguridad"
-        value={value}
+        value={state.value}
         onChange={handleChange}
       />
-      <button onClick={() => setLoading((e) => !e)}>Comprobar</button>
+      <button onClick={() => setState({ ...state, loading: true })}>
+        Comprobar
+      </button>
     </div>
   );
 };
